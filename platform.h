ab4d1c706648ba6435e4a36a08d95c3013a4e705
Lawrence D. Kern <law@lawkern.com>
2025-01-23 19:09:21 -0500

    Add signatures for initialization and frame begin/end functions

--- platform.h
+++ platform.h
@@ -1,8 +1,5 @@
 #pragma once
-
-/* /////////////////////////////////////////////////////////////////////////// */
-/* (c) copyright 2024 Lawrence D. Kern /////////////////////////////////////// */
-/* /////////////////////////////////////////////////////////////////////////// */
+/* (c) copyright 2024 Lawrence D. Kern ////////////////////////////////////// */
 
 // NOTE: Default configurable values.
 
@@ -16,11 +13,28 @@
 #   define WINDOW_HEIGHT 400
 #endif
 
+#include <stdbool.h>
 #include <stddef.h> // ptrdiff_t
 
 // NOTE: This file specifies the API for any functionality provided by the host
 // platform. The function signatures only appear here - each implementation
 // should use the macros below when a signature is required.
+
+// NOTE: Perform any initialization required by the platform, e.g. creating a
+// window, allocating textures, etc.
+#define PLATFORM_INITIALIZE(name) void name(char *title, int width, int height)
+PLATFORM_INITIALIZE(platform_initialize);
+
+// NOTE: Perform any actions that should occur at the beginning of each frame,
+// e.g handling input and other incoming OS events. Return false to indicate the
+// program should quit.
+#define PLATFORM_FRAME_BEGIN(name) bool name(void)
+PLATFORM_FRAME_BEGIN(platform_frame_begin);
+
+// NOTE: Perform any actions that should occur at the end of each frame,
+// e.g. frame time computations.
+#define PLATFORM_FRAME_END(name) void name(void)
+PLATFORM_FRAME_END(platform_frame_end);
 
 // NOTE: Output a printf-style formatted string to some reasonable location for
 // a given platform.
